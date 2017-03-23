aadca571a57563f6aeadee1480240f1911d9851a
Yavuz Selim <ygugen@windowslive.com>
2017-03-24 00:00:15 +0300

    Add files via upload

---
+++ src/evraktakip_v2/Hasta.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package evraktakip_v2;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author yavuz
+ */
+public class Hasta {
+    static ArrayList<Hasta> hastalar=new ArrayList<>();
+    static ArrayList<Evrak> gonderilenRapor=new ArrayList<>();
+    static ArrayList<Evrak> gelenRecete=new ArrayList<>();
+    int id;
+    String ad,soyad,pass;
+    
+    Hasta(String ad,String soy,String pass,int id){
+        this.ad=ad;
+        this.soyad=soy;
+        this.pass=pass;
+        this.id=id;
+        hastalar.add(this);
+    }
+}
